dc7588ceac6279235d36209df6ab375e0f36bb8c
Mercy-Muchai <mercy.muchai@andela.com>
2017-09-19 22:48:07 +0300

    run tests method

--- manage.py
+++ manage.py
@@ -11,5 +11,14 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def test():
+    """Runs all tests on one command."""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
 if __name__ == '__main__':
     manager.run()
